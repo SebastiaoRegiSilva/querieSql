5c10728f893f7071c28f74fa985f346b63fc6076
Sebastião Willian Pereira Regi <84387616+SebastiaoRegiSilva@users.noreply.github.com>
2022-05-04 21:47:00 -0300

    Hospedagem gratuita...

--- querieAula10.sql
+++ querieAula10.sql
@@ -22,16 +22,19 @@ CREATE TABLE cliente(
 -- CRUD para operações mobile, a sintaxe terá que ser adequada para o uso do SQLite.
 -- CREATE / INSERT.
 insert into cliente values 
-(null, 'Zé da Pena', '12345678', 'Rua das Pendangas', '144', 'Hadouken', 'Vaquejada', 'São Tomás de Aquino', 'MG', 61461.00, 8787.00),
-(null, 'Bruno Kandry', '13188030', 'Rua Carlos Chagas', '666', 'Malagasy is good', 'Jardim Amanda', 'Hortolândia', 'SP', 5000.00, 1045.00),
-(null, 'Sebastião Willian', '29161112', 'Rua Paraíso', '70', 'IPB Jardim Tropical', 'Jardim Tropical', 'Serra', 'ES', 7000.00, 5412.00);
+('Zé da Pena', '12345678', 'Rua das Pendangas', '144', 'Hadouken', 'Vaquejada', 'São Tomás de Aquino', 'MG', 61461.00, 8787.00),
+('Bruno Kandry', '13188030', 'Rua Carlos Chagas', '666', 'Malagasy is good', 'Jardim Amanda', 'Hortolândia', 'SP', 5000.00, 1045.00),
+('Sebastião Willian', '29161112', 'Rua Paraíso', '70', 'IPB Jardim Tropical', 'Jardim Tropical', 'Serra', 'ES', 7000.00, 5412.00);
 
 -- READ / SELECT. #Buscar todos cadastrados.
 select * from cliente;
 
 -- UPDATE
 -- Fazer validação se o novo código é existente.
-UPDATE cliente set nome = 'João da Perereca', cep = '87654321', endereco = 'Rua do bobos', numero = 0, complemento = 'Ninguém podia dormir', bairro = 'Imaginário', cidade = 'Fantasma', estado = 'BA', rendaMensal = 0, limiteCredito = 0 where codigo = 1;  
+UPDATE cliente set nome = 'João Perereca', cep = '87654321', endereco = 'Rua do bobos', numero = 0, complemento = 'Ninguém podia dormir', bairro = 'Imaginário', cidade = 'Fantasma', estado = 'BA', rendaMensal = 0, limiteCredito = 0 where codigo = 1;  
 
 -- DELETE - Excluir cliente com base no código.
 DELETE from cliente where codigo = 1;
+
+-- Hospedagem gratuita
+'https://www.db4free.net/'
